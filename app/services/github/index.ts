8a8b79e206e1db8369b4cf0eb0e10d86a421a580
Diego Buriti <bs.diego97@gmail.com>
2022-01-05 20:51:12 -0300

    chore: add a service to get user information on github

---
+++ app/services/github/index.ts
@@ -0,0 +1,33 @@
+export type UserTypes = {
+  name: string
+  login: string
+  avatar_url: string
+  html_url: string
+  bio: string
+  location: string
+  followers: string
+  following: string
+}
+
+export type LoaderData = {
+  user: UserTypes
+}
+
+export const getGithubProfile = async (username: string) => {
+  const res = await fetch(`https://api.github.com/users/${username}`, {
+    headers: {
+      accept: 'application/vnd.github.v3.json',
+      Authorization: `token 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`
+    }
+  })
+  const { bio, name, avatar_url, html_url, location, login }: UserTypes = await res.json()
+
+  return {
+      avatar_url,
+      bio,
+      name,
+      html_url,
+      location,
+      login
+  }
+}
