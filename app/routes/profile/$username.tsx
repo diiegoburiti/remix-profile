903d01116c3166dcf12e9730132d3d89bfc67187
Diego Buriti <bs.diego97@gmail.com>
2022-01-12 22:06:27 -0300

    feat: add CatchBoundary to get response errors  api errors

--- app/routes/profile/$username.tsx
+++ app/routes/profile/$username.tsx
@@ -1,12 +1,41 @@
-import { LoaderFunction, useLoaderData, redirect, ActionFunction } from 'remix'
+import { Heading, Button } from '@chakra-ui/react'
+import {
+  LoaderFunction,
+  useLoaderData,
+  useCatch,
+  Link
+} from 'remix'
 import { Summary } from '~/components/sumamry'
 import { Wrapper } from '~/components/wrapper'
 import { getGithubProfile, LoaderData } from '~/services/github'
+import { ArrowBackIcon } from '@chakra-ui/icons'
 
 export const loader: LoaderFunction = async ({ params }) => {
   return {
     user: await getGithubProfile(String(params.username))
   }
+}
+
+export function CatchBoundary() {
+  const caught = useCatch()
+  return (
+    <Wrapper
+      d='flex'
+      flexDir='column'
+      alignItems='center'
+      mt='20vh'
+
+    >
+      <Heading as="h1" size="3xl" color="fontColor" textAlign="center" mb='10vh'>
+        {caught.data}
+      </Heading>
+
+      <Button bg="borderColor" color="fontColor" d='flex' justifyContent='space-between' w='8vw'>
+        <ArrowBackIcon w={5}/>
+        <Link to="/">Back</Link>
+      </Button>
+    </Wrapper>
+  )
 }
 
 export default function () {
