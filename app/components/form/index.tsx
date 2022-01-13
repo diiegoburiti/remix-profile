5807a87b32fb049340c84c2f8e4bf8a6de694256
Diego Buriti <bs.diego97@gmail.com>
2022-01-12 22:03:26 -0300

    refactor: refactor form component

--- app/components/form/index.tsx
+++ app/components/form/index.tsx
@@ -16,28 +16,39 @@ export const Form = ({ isLoading }: FormProps) => {
 
   return (
     <RemixForm method="post">
-      <FormControl>
-        <FormLabel htmlFor="username">First name</FormLabel>
+      <FormControl position="relative">
         <Input
+          aria-label="input to search for a profile on github"
           id="username"
           name="username"
-          placeholder="Search for a user on github"
+          placeholder="Search for a user on github."
           onChange={handleChange}
           value={inputValue}
+          color="fontColor"
+          border="2px solid white"
+          _focus={{
+            borderColor: 'borderColor'
+          }}
         />
+        <Button
+          colorScheme="teal"
+          fontSize={28}
+          justifyContent="space-between"
+          right="0px"
+          position="absolute"
+          bg="borderColor"
+          border='2px solid borderColor'
+          borderLeftRadius="0"
+          _hover={{
+            opacity: '50%'
+          }}
+          isLoading={isLoading}
+          type="submit"
+          disabled={!inputValue}
+        >
+          <Search2Icon w={5} h={5} />
+        </Button>
       </FormControl>
-      <Button
-        mt={4}
-        w={40}
-        colorScheme="teal"
-        fontSize={28}
-        justifyContent="space-between"
-        isLoading={isLoading}
-        type="submit"
-      >
-        <span>Search</span>
-        <Search2Icon w={5} h={5} />
-      </Button>
     </RemixForm>
   )
 }
