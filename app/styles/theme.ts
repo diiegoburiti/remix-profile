aac63282475b2c961af79b94bdd78e7cf2bfc6e4
Diego Buriti <bs.diego97@gmail.com>
2022-03-23 20:50:07 -0300

    chore: add breakpoints sizes

--- app/styles/theme.ts
+++ app/styles/theme.ts
@@ -1,17 +1,24 @@
-import { extendTheme} from '@chakra-ui/react'
+import { extendTheme } from "@chakra-ui/react";
 
 export const theme = extendTheme({
   colors: {
-    mainBg: '#171923',
-    bg: '#2D3748',
-    fontColor: '#ebf8ff',
-    borderColor: '#D53F8C'
+    mainBg: "#171923",
+    bg: "#2D3748",
+    fontColor: "#ebf8ff",
+    borderColor: "#D53F8C",
   },
   styles: {
     global: () => ({
       body: {
-        background: 'mainBg'
-      }
-    })
-  }
-})
+        background: "mainBg",
+      },
+    }),
+  },
+  breakpoints: {
+    sm: "30em",
+    md: "48em",
+    lg: "62em",
+    xl: "80em",
+    "2xl": "96em",
+  },
+});
